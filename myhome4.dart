52de076562c7cd40d53575114f1181b27108492a
Chuthithep <143580407+ChuthithepYuait@users.noreply.github.com>
2026-07-10 15:08:22 +0700

    fix My home 4

---
+++ myhome4.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class MyHome4 extends StatefulWidget {
+  const MyHome4({super.key});
+
+  @override
+  State<MyHome4> createState() => _MyHome4State();
+}
+
+class _MyHome4State extends State<MyHome4> {
+  int _len = 0;
+  static const int _max = 100;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("MyHome4"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            TextField(
+              maxLines: 3,
+              onChanged: (t) {
+            setState(() {
+              _len = t.length;
+            });
+          },
+          decoration: const InputDecoration(
+            hintText: 'พิมพ์ข้อความ...',
+            border: OutlineInputBorder(),
+          ),
+        ),
+        const SizedBox(height: 4),
+        Text(
+          '$_len / $_max',
+          style: TextStyle(
+            color: _len > _max ? Colors.red : Colors.grey,
+          ),
+        ),
+      ],
+      ),
+    ),
+    );
+  }
+}
